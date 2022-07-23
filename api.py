72efcba0c43645eda9cd19ded17deb292afeb245
Your Name <you@example.com>
2022-07-23 16:25:46 +0800

    api

--- api.py
+++ api.py
@@ -1,1 +1,7 @@
-url = 'https://www.alphavantage.co/'
+import requests
+api_key = 'M3TUAJLWM4IJG8U4'
+url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}"
+response = requests.get(url)
+data=response.json()
+# 'Realtime Currency Exchange Rate'
+print(data)
