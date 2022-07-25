492ee7bc182ca56766d283e7d740e80f80e40e09
Your Name <you@example.com>
2022-07-25 22:41:47 +0800

    .

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,4 +1,11 @@
 from pathlib import Path
+import csv
 
 fp = Path.cwd()/'csv_reports'/'cash-on-hand-thb.csv'
 print(fp.exists())
+
+with fp.open(mode='r',encoding='UTF-8', newline='') as file:
+    reader = csv.reader(file)
+    next(reader)
+    for line in reader:
+        print(line)
--- overheads.py
+++ overheads.py
@@ -1,4 +1,15 @@
 from pathlib import Path
+import csv
 
 fp = Path.cwd()/'csv_reports'/'overheads-day-45.csv'
 print(fp.exists())
+
+with fp.open(mode='r',encoding='UTF-8', newline='') as file:
+    reader = csv.reader(file)
+    next(reader)
+    for line in reader:
+        print(line)
+
+# for data in text:
+
+# print(text)
