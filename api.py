20abb4b97d7157516b22112a1e961a38ec79544d
Your Name <you@example.com>
2022-07-26 13:27:32 +0800

    done api

--- api.py
+++ api.py
@@ -1,7 +1,15 @@
 import requests
+
 api_key = 'M3TUAJLWM4IJG8U4'
+
+# the API URL link for exchange rate from USD to SGD
 url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}"
+
+# to request access to the data and assigned it to a object response
 response = requests.get(url)
-data=response.json()
-# 'Realtime Currency Exchange Rate'
-print(data)
+
+# retrieve data with .json from response and save it to a object data
+data = response.json()
+exchange_rate = data['Realtime Currency Exchange Rate']
+forex = exchange_rate['5. Exchange Rate']
+print(forex)
