5c4089b3e3a02a323f7e98c4f644bf2ef5d760b2
Your Name <you@example.com>
2022-07-27 23:19:25 +0800

    .

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -8,4 +8,5 @@ with fp.open(mode='r',encoding='UTF-8', newline='') as file:
     reader = csv.reader(file)
     next(reader)
     for line in reader:
-        print(line)
+        print(line[1])
+        
