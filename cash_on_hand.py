8dab834cfec38f0a134dc693873386c6977be22a
Your Name <you@example.com>
2022-07-30 23:16:10 +0800

    .

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -3,10 +3,12 @@ import csv
 
 fp = Path.cwd()/'csv_reports'/'cash-on-hand-usd.csv'
 print(fp.exists())
-
+empty_list = []
+pos = 0
 with fp.open(mode='r',encoding='UTF-8', newline='') as file:
     reader = csv.reader(file)
     next(reader)
     for line in reader:
-        print(line[1])
-        
+        empty_list.append(float(line[1]))
+
+print(empty_list)
--- profit_loss.py
+++ profit_loss.py
@@ -2,15 +2,19 @@ from pathlib import Path
 import csv
 
 fp = Path.cwd()/'csv_reports'/'profit-and-loss-usd.csv'
-print(fp.exists())
+
 empty_list = []
+day_list = []
+position = 0
 with fp.open(mode='r',encoding='UTF-8', newline='') as file:
     reader = csv.reader(file)
     next(reader)
     for line in reader:
-        empty_list.append(line[4])
-
-
-
-print(empty_list)        
+        day_list.append(line[0])
+        empty_list.append(float(line[4]))
+        # if empty_list[0] > empty_list[1]:
+        #     print(day_list)
+print(day_list)
+print(empty_list)
 print(empty_list[1])
+
