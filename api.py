a9e74d90927bb016baf9fb2774bad42bd7c33d51
Your Name <you@example.com>
2022-07-31 15:05:55 +0800

    .

--- api.py
+++ api.py
@@ -12,15 +12,14 @@ response = requests.get(url)
 # retrieve data with .json from response and assigned it to a object data
 data = response.json()
 
-print(data)
 # gets the data of realtime currency exchange rate and assigned to an object exchange rate
 exchange_rate_dict = data['Realtime Currency Exchange Rate']
 
 # gets the exchange rate from the dictionary and stores the value in an object forex
 forex = exchange_rate_dict['5. Exchange Rate']
 
-# 
+# stores a message for real time currency conversion rate from usd to sgd using f-strings
 api_message = f'[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD{forex}'
 
-# shows the value of the exchange rate 
+# shows the real time currency exchange rate from USD to SGD
 print(api_message)
