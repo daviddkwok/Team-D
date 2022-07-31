256919e2c05b78dfd1bc67a7b7382583b7378110
Your Name <you@example.com>
2022-07-31 14:18:33 +0800

    documentation of overheads

--- overheads.py
+++ overheads.py
@@ -1,20 +1,45 @@
 from pathlib import Path
 import csv
 
+# assigning the file path of overheads csv to fp
 fp = Path.cwd()/'csv_reports'/'overheads-day-40.csv'
 
+# creating two empty lists for empty list and overhead
 empty_list = []
-overhead = []
-with fp.open(mode='r',encoding='UTF-8', newline='') as file:
-    reader = csv.reader(file)
-    next(reader)
-    for line in reader:
-        empty_list.append(float(line[1]))
-        overhead.append(line[0])
-max_amount = max(empty_list)
-index = empty_list.index(max_amount)
-overheads = (overhead[index])
-upper_overheads = overheads.upper()
+overhead_list = []
 
+# opening csv file to read with a variable 'file'
+with fp.open(mode='r',encoding='UTF-8', newline='') as file:
+    
+    #assign .reader() object to reader to read file
+    reader = csv.reader(file)
+
+    # next() to skip the header
+    next(reader)
+
+    # create for loop for line in text 
+    for line in reader:
+
+        # adds on the overhead categories to the overhead empty list
+        overhead_list.append(line[0])
+
+        # adds on the float overhead amounts to the empty_list
+        empty_list.append(float(line[1]))
+
+# using the max() function to find the maximum value in the empty list and assigns to variable, max_amount
+max_amount = max(empty_list)
+
+# finding the index position of the max_amount value and assigns to variable, index 
+index = empty_list.index(max_amount)
+
+# locates the position of the category by using the index position from the overhead list
+category = (overhead_list[index])
+
+# converts the category to all uppercase
+upper_overheads = category.upper()
+
+# stores a message for highest overheads category and amount using f-strings
 message = f'[HIGHEST OVERHEADS] {upper_overheads}: SGD{max_amount}'
+
+# shows the message
 print(message)
--- profit_loss.py
+++ profit_loss.py
@@ -10,11 +10,15 @@ with fp.open(mode='r',encoding='UTF-8', newline='') as file:
     reader = csv.reader(file)
     next(reader)
     for line in reader:
-        day_list.append(line[0])
+        day_list.append(float(line[0]))
         empty_list.append(float(line[4]))
         # if empty_list[0] > empty_list[1]:
         #     print(day_list)
-print(day_list)
-print(empty_list)
-print(empty_list[1])
+    if empty_list[position]>empty_list[position+1]:
+        print(day_list)
+    
+#     print(empty_list[1])
+# print(day_list)
+# print(empty_list)
+# print(empty_list[1])
 
