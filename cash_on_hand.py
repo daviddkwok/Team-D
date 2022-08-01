fac1f797afbd84e8359e09baf3a535e85b495615
Your Name <you@example.com>
2022-08-01 16:32:53 +0800

    .

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -3,25 +3,33 @@ import csv
 
 
 coh_fp = Path.cwd()/'csv_reports'/'cash-on-hand-usd.csv'
+summary_path = Path.cwd()/'summary_report.txt'
+summary_path.touch()
 
-def cohfunction(forex):
-    
-    empty_list = []
+# def cohfunction(forex):
 
-    with coh_fp.open(mode='r',encoding='UTF-8', newline='') as file:
+day_list = []    
+amount_list = []
+
+with coh_fp.open(mode='r',encoding='UTF-8', newline='') as file:
         reader = csv.reader(file)
         next(reader)
         for line in reader:
-            empty_list.append(float(line[1]))
+            amount_list.append(float(line[1]))
+            day_list.append(float(line[0]))
         index = 0
-        deflict = 0 
-        while index+1<len(empty_list):
-            if float(empty_list[index])>float(empty_list[index+1]):
-                deflict = float(empty_list[index])- float(empty_list[index+1])
-        index = index+1
-    # if deflict == 0:
-    file.close()
-    print(empty_list[0])
+        cash_deflict = 0 
+        while index+1<len(amount_list):
+            if float(amount_list[index])>float(amount_list[index+1]):
+                cash_deflict = float(amount_list[index])- float(amount_list[index+1])
+
+                with summary_path.open(mode='w', encoding='UTF-8') as file:
+                    file.write(f'\n[CASH DEFICIT] DAY: {day_list[index+1]}, AMOUNT: SGD{cash_deflict}')
+            index = index+1
+        if cash_deflict == 0:
+            with summary_path.open(mode='w',encoding='UTF-8') as file:
+                file.write('\n[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY')
+                file.close()
 
 
-    print(empty_list)
+# print(cohfunction(forex))
