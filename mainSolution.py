ce387682cef39d057e79ec57dd210d500d996570
Your Name <you@example.com>
2022-08-03 21:53:05 +0800

    done

--- mainSolution.py
+++ mainSolution.py
@@ -2,8 +2,12 @@ import api, cash_on_hand, overheads, profit_loss
 from pathlib import Path
 from api import forex
 
+# assigning the file path of summary report file to summary_path
 summary_path = Path.cwd()/'summary_report.txt'
+
+# creates a new file with .touch()
 summary_path.touch()
+
 def main():
     api.api_function()
     overheads.overhead_function(forex)
